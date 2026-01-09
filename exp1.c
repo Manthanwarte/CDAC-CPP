7dfd6e7a305594722f7f5ad2d923236b476830b7
Manthanwarte <manthanwarte123@gmail.com>
2026-01-09 22:01:09 +0530

    Day 2

--- exp1.c
+++ exp1.c
@@ -1,51 +1,35 @@
 #include<stdio.h>
 int main()
 {
-    float buyprice, sellprice;
-    int quantity;
+    float num1, num2, result;
+    char operation;
 
-    printf("Enter Buy Price: ");
-    scanf("%f",&buyprice);
+    printf("Enter first number: ");
+    scanf("%f", &num1);
 
-    printf("Enter Sell Price:");
-    scanf("%f",&sellprice);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operation);
 
-    printf("Enter Quantity:");
-    scanf("%d",&quantity);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
 
-    float buyamount = buyprice * quantity ;
-    float sellamount = sellprice * quantity ;
-    float turnover = sellamount + buyamount ;
-
-    float Total_Brokerage = 0.0003f * buyamount + 0.0003f * sellamount;
-
-    float service_tax = 0.1036f * Total_Brokerage;
-
-    float stt = 0.00025f * sellamount;
-
-    float stamp_duty = 0.00002f * turnover;
-
-    float regulatory_charges = 0.00004f * turnover;
-
-    float total_charges = Total_Brokerage + service_tax + stt + stamp_duty + regulatory_charges;
-
-    float gross_profit = sellamount - buyamount;
-    float net_profit = gross_profit - total_charges;
-
-
-
-    printf("Turnover: %.2f\n", turnover);
-    printf("Brokerage: %.2f\n", Total_Brokerage);
-    printf("Service Tax: %.2f\n", service_tax);
-    printf("STT: %.2f\n", stt);
-    printf("Stamp Duty: %.2f\n", stamp_duty);
-    printf("Regulatory Charges: %.2f\n", regulatory_charges);
-    printf("Total Charges: %.2f\n", total_charges);
-    printf("Net Profit: %.2f\n", net_profit);
-    printf("Gross Profit: %.2f\n", gross_profit);
-    return 0;
-
+    if(operation == '+')
+        result = num1 + num2;
+    else if(operation == '-')
+        result = num1 - num2;
+    else if(operation == '*')
+        result = num1 * num2;
+    else if(operation == '/')
+    {
+        if(num2 != 0)
+            result = num1 / num2;
+        else
+        {
+            printf("Error Divided by zero.\n");
+            return 1;
+        }
+    }
     
-
-
+    printf("Result: %.2f\n", result);
+    return 0;
 }
