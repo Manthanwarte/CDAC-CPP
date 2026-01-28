aae02e447b47e7f657291ffc39db1afe61c72459
Manthanwarte <manthanwarte123@gmail.com>
2026-01-28 22:51:17 +0530

    Day 5 exp 1
    Day 5 exp 1

--- exp1.cpp
+++ exp1.cpp
@@ -1,54 +1,30 @@
 #include<iostream>
-#include<iomanip>
 using namespace std;
 
-int main()
-{
-    char Movie_name;
-    string movieName;
-    int adultTicketPrice, childTicketPrice, adultTicketSold, childTicketSold, percentage;
-    float grossAmount, amountDonated, netsale;
-    int noOfTicketsSold;
+typedef struct employee{
+    int empID;
+    char empName[20];
+    float empSalary;
+}emp_t;
 
-    cout << "Enter Movie Name : ";
-    getline(cin, movieName);
-
-    cout << "Enter Adult Ticket Price : ";
-    cin >> adultTicketPrice;
-
-    cout << "Enter Child Ticket Price : ";
-    cin >> childTicketPrice;
-
-    cout << "Enter No. of Adult Ticket Sold : ";
-    cin >> adultTicketSold;
-
-    cout << "Enter No. of Child Ticket Sold : ";
-    cin >> childTicketSold;
-
-    cout << "Percentage of Gross Amount Donated : ";
-    cin >> percentage;
-
-    grossAmount = (adultTicketPrice * adultTicketSold) + (childTicketPrice * childTicketSold);
-    noOfTicketsSold = adultTicketSold + childTicketSold;
-    grossAmount = (adultTicketPrice * adultTicketSold) + (childTicketPrice * childTicketSold);
-    amountDonated = (grossAmount * percentage) / 100;
-    netsale = grossAmount - amountDonated;
-    cout << fixed << setprecision(2);
-
-
-    // cout << left << setw(30) << setfill('.') << movieName << endl;
-    // cout << left << setw(30) << setfill('*') << endl;
-    for(int i = 0 ; i <= 40 ; i++)
-    {
-        cout << "-*";
-    }
-    cout << endl ;
-    cout << left << setw(70) << setfill('.') << "Movie Name : "<< " " << movieName << endl;
-    cout << left << setw(70) << setfill('.') << "Number of Tickets Sold : " << " " << noOfTicketsSold << endl;
-    cout << left << setw(70) << setfill('.') << "Gross Amount : " << " $ " << grossAmount << endl;
-    cout << left << setw(70) << setfill('.') << "Percentage of Gross Amount Donated : " << "  "<< percentage  << endl;
-    cout << left << setw(70) << setfill('.') << "Amount Donated : " << " $ " << amountDonated << endl;
-    cout << left << setw(70) << setfill('.') << "Net Sale : " << " $" << netsale << endl;
+void acceptData(employee *emp){
+    cout << "Enter Employee ID : ";
+    cin >> emp->empID;
+    cout << "Enter Employee Name : ";
+    cin >> emp->empName;
+    cout << "Enter Employee Salary : ";
+    cin >> emp->empSalary;
 }
-                                                    
-                                                                                                                                                                                                                        
+void printData(employee *emp){
+    cout << "Employee ID : " << emp->empID << endl;
+    cout << "Employee Name : " << emp->empName << endl;
+    cout << "Employee Salary : " << emp->empSalary << endl;
+}
+int main(){
+    employee emp;
+
+    acceptData(&emp);
+    printData(&emp);
+    
+    
+}
