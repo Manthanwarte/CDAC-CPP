a1b8943c3f442aa3cfc5cafc88429c31c7ab4aa9
EnoughManthan <manthanwarte123@gmail.com>
2026-02-14 23:17:37 +0530

    Day 7 exp3
    Day 7 exp3

--- exp3.cpp
+++ exp3.cpp
@@ -1,26 +1,48 @@
 #include<iostream>
 using namespace std;
 
-bool is_prime(int n) {
-    if (n <= 1) return false;
-    for (int i = 2; i < n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
+int addition(int a, int b) {
+    return a + b;
+}
+
+float addition(float a, float b) {
+    return a + b;
+}
+
+int addition(int a, int b, int c) {
+    return a + b + c;
+}
+
+float addition(float a, float b, float c) {
+    return a + b + c;
+}
+
+float addition(float a) {
+    return a;
+}
+
+int addition(int a, int b, int c, int d, int e) {
+    return a + b + c + d + e;
+}
+
+float addition(float a, float b, float c, float d) {
+    return a + b + c + d;
+}
+
+float addition(char a, float b, float c, float d) {
+    return static_cast<float>(a) + b + c + d;
 }
 
 int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    
-    if (is_prime(num)) {
-        cout << num << " is a prime number.\n";
-    } else {
-        cout << num << " is not a prime number.\n";
-    }
-    
+    cout << "2 ints: " << addition(5, 3) << endl;
+    cout << "2 floats: " << addition(4.2f, 1.8f) << endl;
+    cout << "3 ints: " << addition(1, 2, 3) << endl;
+    cout << "3 floats: " << addition(1.1f, 2.2f, 3.3f) << endl;
+    cout << "1 float: " << addition(7.5f) << endl;
+    cout << "1 int + 4 ints: " << addition(1, 2, 3, 4, 5) << endl;
+    cout << "4 floats: " << addition(1.1f, 2.2f, 3.3f, 4.4f) << endl;
+    cout << "1 char + 3 floats: " 
+         << addition('A', 1.1f, 2.2f, 3.3f)<< endl;
+
     return 0;
 }
