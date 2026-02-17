20d62c2bae954ab17ee16e3933ee22341c7b88d9
EnoughManthan <manthanwarte123@gmail.com>
2026-02-17 21:48:48 +0530

    Day 7 exp 5
    Day 7 exp 5

--- exp5.cpp
+++ exp5.cpp
@@ -1,31 +1,44 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
-bool is_palindrome(string s) {
+class Box {
+private:
+    float length;
+    float width;
+    float height;
 
-    int left = 0;
-    int right = s.length() - 1;
-    while (left < right) {
-        if (s[left] != s[right]) {
-            return false;
-        }
-        left++;
-        right--;
+public:
+    Box(float len, float w = 4.05f, float h = 5.06f) {
+        length = len;
+        width = w;
+        height = h;
     }
-    return true;
-}
+
+    float calculateVolume() {
+        return length * width * height;
+    }
+
+    void display() {
+        cout << "Box Dimensions:" << endl;
+        cout << "Length: " << length << " units" << endl;
+        cout << "Width: " << width << " units" << endl;
+        cout << "Height: " << height << " units" << endl;
+        cout << "Volume: " << calculateVolume() << " cubic units" << endl;
+    }
+};
 
 int main() {
-    string s;
-    cout << "Enter a string: ";
-    cin >> s;
+    Box box1(2.5f, 3.0f, 4.0f);
+    cout << "Box 1:" << endl;
+    box1.display();
     
-    if (is_palindrome(s)) {
-        cout << "The string is a palindrome" << endl;
-    } else {
-        cout << "The string is not a palindrome" << endl;
-    }
+    Box box2(6.0f);
+    cout << "\nBox 2 (with same width and height):" << endl;
+    box2.display();
+    
+    Box box3(3.0f, 4.5f, 2.0f);
+    cout << "\nBox 3 (with same height):" << endl;
+    box3.display();
     
     return 0;
 }
