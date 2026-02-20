4d94ec8cbce96d46184188e4e258394e0719571a
EnoughManthan <manthanwarte123@gmail.com>
2026-02-20 09:32:32 +0530

    Day 8 exp 1
    Day 8 exp 1

--- exp1.cpp
+++ exp1.cpp
@@ -1,69 +1,62 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-class Time{
+class Chair{
     private:
-        int hour;
-        int minute;
-        int second;
-    
+        int height;
+        int width;
+        double price;
+        string color;
+
     public:
-        Time(){
-            hour = 0;
-            minute = 0;
-            second = 0;
-        }
-        
-        Time(int h, int m, int s){
-            hour = h;
-            minute = m;
-            second = s;
-        }
-        
-        void acceptInput(){
-            cout << "Enter hour: ";
-            cin >> hour;
-            cout << "Enter minute: ";
-            cin >> minute;
-            cout << "Enter second: ";
-            cin >> second;
+        Chair(){
+            height = 0;
+            width = 0;
+            price = 0.0;
+            color = "";
+
         }
 
-        void displayTime(){
-            cout << hour << ":" << minute << ":" << second << endl;
+        Chair(int h , int w , double p, string c){
+            height = h;
+            width = w;
+            price = p;
+            color = c;
         }
+
+        void Input(){
+            cout << "Enter height : ";
+            cin >> height;
+            cout << "Enter width : ";
+            cin >> width;
+            cout << "Enter price : ";
+            cin >> price;
+            cout << "Enter color : ";
+            cin >> color;
+        }
+
+        void Display() {
+            cout << "Height : " << height << endl;
+            cout << "Width : " << width << endl;
+            cout << "Price : " << price << endl;
+            cout << "Color : " << color << endl;
+        }
+
+        static void SetPrice(Chair &chair , double newprice){
+            chair.price = newprice;
+        }
+
+        ~Chair(){
+            cout << "Chair is destructed " << endl;
+        }        
 };
-
 int main()
 {
-    Time t;
-    int choice;
-    
-    cout << " Calculate Time" << endl;
-    cout << "1. Enter time" << endl;
-    cout << "2. Display time" << endl;
-    cout << "3. Exit" << endl;
-    
-    do {
-        cout << "\nEnter your choice (1-3): ";
-        cin >> choice;
-        
-        switch(choice){
-            case 1:
-                t.acceptInput();
-                cout << "Time saved successfully!" << endl;
-                break;
-            case 2:
-                cout << "Current time: ";
-                t.displayTime();
-                break;
-            case 3:
-                cout << "Exit" << endl;
-                break;
-            default:
-                cout << "Invalid choice! Please try again." << endl;
-        }
-    } while(choice != 3);
-    
+    Chair chair1;
+    chair1.Input();
+    cout << "\nChair:" << endl;
+    chair1.Display();
+
     return 0;
 }
